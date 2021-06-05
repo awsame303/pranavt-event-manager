9044cda56eecd36bcf4d4b39d0e135285740076d
awsame303 <72770606+awsame303@users.noreply.github.com>
2021-06-05 09:19:33 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Header from "./components/Header.svelte";
+
+	const events = [
+		{
+			id: "1",
+			title: "Programming Bootcamp",
+			description: "A bootcamp to learn a new language",
+			address: "27th NerdStreet",
+			contact: "0000000000",
+			fees: "$25"
+		},
+		{
+			id: "2",
+			title: "Webdesign",
+			description: "A web design competition",
+			address: "27th NerdStreet",
+			contact: "0000000000",
+			fees: "$25"
+		}
+	]
+</script>
+
+<Header />
---
+++ src/Events/EventItems.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let title;
+  export let description;
+  export let address;
+  export let contact;
+  export let fees;
+</script>
+
+<div>
+  <header>
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </header>
+  <div class="content">
+    <p>{address}</p>
+    <p>{contact}</p>
+    <p>{fees}</p>
+  </div>
+</div>
